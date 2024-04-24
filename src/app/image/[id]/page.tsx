257c5259a81ba342193329d8ebc9d8d8340af39e
afyqeyzar <norafyqe@gmail.com>
2024-04-24 15:46:18 +0100

    feat: add individual image page

--- src/app/image/[id]/page.tsx
+++ src/app/image/[id]/page.tsx
@@ -5,18 +5,41 @@ import { useEffect, useState } from "react";
 
 export default function ImagePage() {
   const { id } = useParams();
-  const [images, setImages] = useState({});
+  const [image, setImage] = useState<{
+    name: string;
+    filename: string;
+    description: string;
+  } | null>(null);
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios.get(`http://localhost:8080/files/${id}`);
 
-      setImages(data);
+      setImage(data);
     };
     getData();
   }, []);
+
+  if (!image) {
+    return <p>Loading...</p>;
+  }
   return (
-    <>
-      <h3>this is {id}</h3>
-    </>
+    <main className="flex justify-around">
+      <img
+        src={`http://localhost:8080/image/${image.filename}`}
+        alt={image.name}
+        className="flex-1"
+      />
+      <section className=" flex justify-center flex-col items-start flex-1 p-4">
+        <h3 className=" font-thin text-5xl mb-5">{image.name}</h3>
+        <p className="mb-8">{image.description}</p>
+
+        <button className="px-2 py-1 border-2 border-black rounded mb-2">
+          Edit
+        </button>
+        <button className="px-2 py-1 rounded bg-red-700 text-white">
+          Delete
+        </button>
+      </section>
+    </main>
   );
 }
